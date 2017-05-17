9e115658ef065fecc1d6ddf462d8b3e024d917ab
liweizhi <li41898@163.com>
2017-05-18 01:04:38 +0800

    no message

--- test.go
+++ test.go
@@ -4,33 +4,37 @@ package main
 import (
 
 
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/disk"
-	"fmt"
+	//"github.com/shirou/gopsutil/cpu"
+	//"github.com/shirou/gopsutil/host"
+	//"github.com/shirou/gopsutil/mem"
+	//"github.com/shirou/gopsutil/disk"
+	//"fmt"
+	//"github.com/shirou/gopsutil/cpu"
+	//"github.com/shirou/gopsutil/host"
+	//"github.com/shirou/gopsutil/mem"
+	//"github.com/shirou/gopsutil/disk"
 )
 
 func main() {
 
-	cs,_ := cpu.Info()
-
-	for _, c := range cs{
-
-		fmt.Println(c.ModelName)
-	}
-
-	host, _ := host.Info()
-	fmt.Println(host.Hostname, host.String())
-
-	swapmem, _ := mem.SwapMemory()
-	fmt.Println(swapmem.Total)
-	virtual, _ := mem.VirtualMemory()
-	fmt.Println(virtual.Total)
-
-	disk, _ := disk.Usage("/Volumes/Macintosh HD")
-	fmt.Println(disk.Total)
-
+	//cs,_ := cpu.Info()
+	//
+	//for _, c := range cs{
+	//
+	//	fmt.Println(c.ModelName)
+	//}
+	//
+	//host, _ := host.Info()
+	//fmt.Println(host.Hostname, host.String())
+	//
+	//swapmem, _ := mem.SwapMemory()
+	//fmt.Println(swapmem.Total)
+	//virtual, _ := mem.VirtualMemory()
+	//fmt.Println(virtual.Total)
+	//
+	//disk, _ := disk.Usage("/Volumes/Macintosh HD")
+	//fmt.Println(disk.Total)
+	//
 
 
 
